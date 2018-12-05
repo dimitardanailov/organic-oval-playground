c2ebbc0777f005f19e63111b61c33f6023c83a7d
Dimitar Danailov <dimityr.danailov@gmail.com>
2018-12-05 18:58:30 +0200

    Project has first simple oval component.

--- src/index.js
+++ src/index.js
@@ -1,4 +1,21 @@
 var oval = require('organic-oval')
 oval.init()
-require('./index.tag')
+
+class Component {
+  constructor (tagName, root) {
+    oval.BaseTag(this, tagName, root)
+  }
+
+  render (createElement) {
+    return createElement(
+      'h1',
+      {
+				style: 'color: green; text-align: center'
+			},
+      'Hello Organic World!'
+    )
+  }
+}
+
+oval.registerTag('app', Component)
 oval.mountAll(document.body)
